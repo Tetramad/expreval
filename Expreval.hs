cffdc3049d51e9f5da98d32d6caa10afff36e228
Tetramad <cbwjd777@gmail.com>
2020-08-25 01:32:25 +0900

    feat: adding mul, div, parenthesis and refactoring
    Signed-off-by: Tetramad <cbwjd777@gmail.com>

--- Expreval.hs
+++ Expreval.hs
@@ -1,68 +1,99 @@
 module Expreval (
+    expreval
 ) where
 
 import Data.Char
 
 data Operator = Addition
+              | Subtraction
               | Multiplication
+              | Division
     deriving (Show)
 
-data ExpressionTree = OperatorNode Operator ExpressionTree ExpressionTree
-                    | ValueNode Integer
+data OpenClose = Open
+               | Close
     deriving (Show)
+
+data Token = Operator Operator
+           | Number Integer
+           | Parenthesis OpenClose
+    deriving (Show)
+
+precedence :: Token -> Integer
+precedence (Parenthesis _) = 0
+precedence (Number _) = 1
+precedence (Operator Multiplication) = 3
+precedence (Operator Division) = 3
+precedence (Operator Addition) = 4
+precedence (Operator Subtraction) = 4
+
+toNumber :: String -> Token
+toNumber s = Number (read s)
+
+tokenize :: String -> [Token]
+tokenize [] = []
+tokenize s
+    | isDigit $ head s =
+        let (ds, rs) = span isDigit s
+        in toNumber ds : tokenize rs
+    | isSpace $ head s = tokenize $ dropWhile isSpace s
+    | '+' == head s =
+        let rs = tail s
+        in Operator Addition : tokenize rs
+    | '-' == head s =
+        let rs = tail s
+        in Operator Subtraction : tokenize rs
+    | '*' == head s =
+        let rs = tail s
+        in Operator Multiplication : tokenize rs
+    | '/' == head s =
+        let rs = tail s
+        in Operator Division : tokenize rs
+    | '(' == head s =
+        let rs = tail s
+        in Parenthesis Open : tokenize rs
+    | ')' == head s =
+        let rs = tail s
+        in Parenthesis Close : tokenize rs
+
+shuntingYard :: [Token] -> [Token]
+shuntingYard [] = []
+shuntingYard xs = shuntingYard' xs [] [] where
+    shuntingYard' [] [] zs = zs
+    shuntingYard' [] ys zs = zs ++ ys
+    shuntingYard' (Parenthesis Open : xs') ys zs = shuntingYard' xs' (Parenthesis Open:ys) zs
+    shuntingYard' (Parenthesis Close : xs') ys zs = shuntingYard' xs' ys' zs' where
+        popCondition (Parenthesis _) = False
+        popCondition _ = True
+        (zs'', ys'') = span popCondition ys
+        ys' = tail ys''
+        zs' = zs ++ zs''
+    shuntingYard' (x':xs') ys zs = shuntingYard' xs' (x':ys') zs' where
+        popCondition (Parenthesis _) = False
+        popCondition a = precedence a <= precedence x'
+        (zs'', ys'') = span popCondition ys
+        ys' = ys''
+        zs' = zs ++ zs''
+
+data ExpressionTree = BinaryNode Token ExpressionTree ExpressionTree
+                    | ValueNode Token
+    deriving (Show)
+
+treefy :: [Token] -> ExpressionTree
+treefy ts = treefy' ts [] where
+    treefy' [] [] = ValueNode (Number 0)
+    treefy' [] (r:_) = r
+    treefy' (x':xs') rs = case x' of
+        Number _ -> treefy' xs' (ValueNode x' : rs)
+        Operator _ -> let (fst:snd:rst) = rs
+                      in treefy' xs' (BinaryNode x' snd fst : rst)
 
 eval :: ExpressionTree -> Integer
-eval (ValueNode a) = a
-eval (OperatorNode Addition a b) = eval a + eval b
-eval (OperatorNode Multiplication a b) = eval a * eval b
+eval (ValueNode (Number x)) = x
+eval (BinaryNode (Operator Addition) a b) = eval a + eval b
+eval (BinaryNode (Operator Subtraction) a b) = eval a - eval b
+eval (BinaryNode (Operator Multiplication) a b) = eval a * eval b
+eval (BinaryNode (Operator Division) a b) = div (eval a) (eval b)
 
-data ExpressionToken = OperatorToken Operator
-                     | ValueToken Integer
-    deriving (Show)
-
-prec :: ExpressionToken -> Integer
-prec (ValueToken _) = 0
-prec (OperatorToken Multiplication) = 1
-prec (OperatorToken Addition) = 2
-
-
-tokenize :: String -> [ExpressionToken]
-tokenize s
-    | null s = []
-    | isDigit $ head s =
-        let (h, t) = span isDigit s
-        in ValueToken (read h) : (tokenize t)
-    | isSpace $ head s = tokenize (dropWhile isSpace s)
-    | '+' == head s = OperatorToken Addition : (tokenize $ tail s)
-    | '*' == head s = OperatorToken Multiplication : (tokenize $ tail s)
-
-intopost :: [ExpressionToken] -> [ExpressionToken]
-intopost ts = intopost' ts [] []
-
-intopost' :: [ExpressionToken] -> [ExpressionToken] -> [ExpressionToken] -> [ExpressionToken]
-intopost' [] [] rs = rs
-intopost' [] os rs = rs ++ os
-intopost' (x:xs) os rs = case x of
-    ValueToken _ -> intopost' xs os (rs ++ [x])
-    OperatorToken Multiplication -> intopost' xs (x : os) rs
-    OperatorToken Addition -> intopost' xs oo rr where
-        c t = prec t < prec x
-        (r, o) = span c os
-        oo = x : o
-        rr = rs ++ r
-
-posttotree :: [ExpressionToken] -> ExpressionTree
-posttotree [] = ValueNode 0
-posttotree xs = posttotree' xs [] where
-    posttotree' [] stk = head stk
-    posttotree' (x' : xs') stk = case x' of
-        ValueToken v -> posttotree' xs' (ValueNode v : stk)
-        OperatorToken o ->
-            let (fst:snd:rst) = stk
-            in posttotree' xs' (OperatorNode o fst snd : rst)
-
-split :: (Char -> Bool) -> String -> [String]
-split p s = case dropWhile p s of
-                "" -> []
-                s' -> w : split p s''
-                      where (w, s'') = break p s'
+expreval :: String -> Integer
+expreval = eval . treefy . shuntingYard . tokenize
